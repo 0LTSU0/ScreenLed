96bbdb37df4d3ca92ac1185279304a7fec502b53
0LTSU0 <55877194+0LTSU0@users.noreply.github.com>
2021-11-06 19:36:10 +0200

    Update raspb.py

--- raspb.py
+++ raspb.py
@@ -1,7 +1,17 @@
 import socket
 from sys import byteorder
+from rp1_ws281x import *
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+
+LED_COUNT = 20
+LED_PIN = 18
+LED_FREQ_HZ = 800000
+LED_DMA = 10
+LED_BRIGHTNESS = 255
+LED_INVERT = False
+LED_CHANNEL = 0
+
+HOST = '192.168.1.3'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 def func():
@@ -16,8 +26,8 @@ def func():
             data = conn.recv(1024)
             if data:
                 decoded = decode(data)
-                print(decoded)
-                #DO LED STUFF
+                #print(decoded)
+                LED(decoded)
 
 def decode(data):
     res = []
@@ -25,6 +35,15 @@ def decode(data):
         res.append(d)
     return(res)
     
+def LED(data):
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
+    segs = len(data)
+    j = 0
+    for i in range.numPixels()):
+        strip.setPixelColor(i, Color(data[j], data[j+1], data[j+2]))
+        j += 3
+        strip.show()
 
 
 func()
