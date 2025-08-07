23583c8fc24d4006c2cf4433daa4619fb908153b
0LTSU0 <lirotus@gmail.com>
2025-08-08 00:18:06 +0300

    linux screenshotting that probably works?

--- ScreenLedLib/ScreenCapLinux.cpp
+++ ScreenLedLib/ScreenCapLinux.cpp
@@ -1,19 +1,92 @@
 #include "ScreenCapLinux.h"
 
+extern "C" {
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+}
+
+
 void screenCaptureWorkerLinux::initScreenShotting() {
-    return;
+    Display* display = XOpenDisplay(nullptr);
+    if (!display) {
+        std::cerr << "Failed to open X11 display!" << std::endl;
+        return;
+    }
+
+    Window root = DefaultRootWindow(display);
+    XWindowAttributes gwa;
+    XGetWindowAttributes(display, root, &gwa);
+
+    m_display = static_cast<void*>(display);
+    m_rootWindow = root;
 }
 
 void screenCaptureWorkerLinux::deinitScreenShotting() {
-    return;
+    if (m_image) {
+        XDestroyImage(static_cast<XImage*>(m_image));
+        m_image = nullptr;
+    }
+
+    if (m_display) {
+        XCloseDisplay(static_cast<Display*>(m_display));
+        m_display = nullptr;
+    }
 }
 
 void screenCaptureWorkerLinux::takeScreenShot() {
-    return;
+    if (!m_display) return;
+
+    Display* display = static_cast<Display*>(m_display);
+    Window root = static_cast<Window>(m_rootWindow);
+
+    int thirdY = m_conf.c_screenResY / 3;
+    if (m_image) {
+        XDestroyImage(static_cast<XImage*>(m_image));
+        m_image = nullptr;
+    }
+
+    XImage* image = XGetImage(display, root, 0, thirdY, m_conf.c_screenResY, thirdY, AllPlanes, ZPixmap);
+    m_image = static_cast<void*>(image);
 }
 
 void screenCaptureWorkerLinux::analyzeColors() {
-    return;
+    XImage* image = static_cast<XImage*>(m_image);
+    if (!image || !image->data){
+        return;
+    }
+
+    m_rgbData.clear();
+    m_rgbData.assign(NUM_LED_SEGMENTS, rgbValue{});
+
+    int xPerSegment = m_conf.c_screenResX / NUM_LED_SEGMENTS;
+    int currentSegment = 0;
+    int itemsPerCell = 0;
+    for (int y = 0; y < image->height; y += 10) {
+        currentSegment = 0;
+        for (int x = 0; x < image->width; x += 4) {
+            if (x % xPerSegment == 0 && x != 0) {
+                currentSegment++;
+            }
+            if (currentSegment == 0) {
+                itemsPerCell++;
+            }
+
+            unsigned long pixel = XGetPixel(image, x, y);
+            int red   = (pixel & image->red_mask)   >> 16;
+            int green = (pixel & image->green_mask) >> 8;
+            int blue  = (pixel & image->blue_mask);
+
+            m_rgbData[currentSegment].r += red;
+            m_rgbData[currentSegment].g += green;
+            m_rgbData[currentSegment].b += blue;
+        }
+    }
+
+    for (int i = 0; i < NUM_LED_SEGMENTS; ++i) {
+        m_rgbData[i].r /= itemsPerCell;
+        m_rgbData[i].g /= itemsPerCell;
+        m_rgbData[i].b /= itemsPerCell;
+    }
 }
 
 void screenCaptureWorkerLinux::sendRGBData(const char* buf) {
--- ScreenLedLib/ScreenCapLinux.h
+++ ScreenLedLib/ScreenCapLinux.h
@@ -20,4 +20,11 @@ public:
     void sendRGBData(const char* buf);
     bool openUDPPort();
     bool closeUDPPort();
+
+private:
+    // X11 and QT have some annoying macro conflicts and the only way I could get it to work is to
+    // include the X11 headers in the source file -> cannot use correct types here
+    void* m_display = nullptr;      // actually Display*
+    unsigned long m_rootWindow = 0; // actually Window
+    void* m_image = nullptr;        // actually XImage*
 };
